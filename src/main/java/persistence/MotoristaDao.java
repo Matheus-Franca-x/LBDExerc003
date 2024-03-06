1c62075dbdf8b50fff96ac88f71bb5daca327425
Matheus França <matheussss6613@gmail.com>
2024-03-06 04:17:59 -0300

    Persistence ok

--- src/main/java/persistence/MotoristaDao.java
+++ src/main/java/persistence/MotoristaDao.java
@@ -1,6 +1,10 @@
 package persistence;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import model.Motorista;
@@ -16,31 +20,91 @@ public class MotoristaDao implements ICrud<Motorista>
 	@Override
 	public void cadastrar(Motorista m) throws SQLException, ClassNotFoundException 
 	{
-		
+		Connection c = gDao.getConnection();
+		String sql = "INSERT INTO motorista VALUES (?, ?, ?)";
+		PreparedStatement ps = c.prepareStatement(sql);
+		ps.setInt(1, m.getCodigo());
+		ps.setString(2, m.getNome());
+		ps.setString(3, m.getNaturalidade());
+		ps.execute();
+		ps.close();
+		c.close();
 	}
 
 	@Override
 	public void alterar(Motorista m) throws SQLException, ClassNotFoundException 
 	{
-		
+		Connection c = gDao.getConnection();
+		String sql = "UPDATE motorista SET nome = ?, naturalidade = ? WHERE codigo = ?";
+		PreparedStatement ps = c.prepareStatement(sql);
+		ps.setString(1, m.getNome());
+		ps.setString(2, m.getNaturalidade());
+		ps.setInt(3, m.getCodigo());
+		ps.execute();
+		ps.close();
+		c.close();
 	}
+	
 
 	@Override
 	public void excluir(Motorista m) throws SQLException, ClassNotFoundException 
 	{
-		
+		Connection c = gDao.getConnection();
+		String sql = "DELETE motorista WHERE codigo = ?";
+		PreparedStatement ps = c.prepareStatement(sql);
+		ps.setInt(1, m.getCodigo());
+		ps.execute();
+		ps.close();
+		c.close();
 	}
 
 	@Override
 	public Motorista consultar(Motorista m) throws SQLException, ClassNotFoundException
 	{
-		return null;
+		Connection c = gDao.getConnection();
+		String sql = "SELECT codigo, nome, naturalidade FROM motorista WHERE codigo = ?";
+		PreparedStatement ps = c.prepareStatement(sql);
+		ps.setInt(1, m.getCodigo());
+		ResultSet rs = ps.executeQuery();
+		
+		if(rs.next())
+		{
+			m.setCodigo(rs.getInt("codigo"));
+			m.setNome(rs.getString("nome"));
+			m.setNaturalidade(rs.getString("naturalidade"));
+		}
+		
+		rs.close();
+		ps.close();
+		c.close();
+		
+		return m;
 	}
 
 	@Override
 	public List<Motorista> list() throws SQLException, ClassNotFoundException
 	{
-		return null;
+		List<Motorista> motoristas = new ArrayList<Motorista>();
+		Connection c = gDao.getConnection();
+		String sql = "SELECT codigo, nome, naturalidade FROM motorista";
+		PreparedStatement ps = c.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		
+		while(rs.next())
+		{
+			Motorista m = new Motorista();
+			m.setCodigo(rs.getInt("codigo"));
+			m.setNome(rs.getString("nome"));
+			m.setNaturalidade(rs.getString("naturalidade"));
+			
+			motoristas.add(m);
+		}
+		
+		rs.close();
+		ps.close();
+		c.close();
+		
+		return motoristas;
 	}
 
 }
--- src/main/java/persistence/ViagemDao.java
+++ src/main/java/persistence/ViagemDao.java
@@ -1,8 +1,14 @@
 package persistence;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import model.Motorista;
+import model.Onibus;
 import model.Viagem;
 
 public class ViagemDao implements ICrud<Viagem>
@@ -20,31 +26,143 @@ public class ViagemDao implements ICrud<Viagem>
 	@Override
 	public void cadastrar(Viagem v) throws SQLException, ClassNotFoundException 
 	{
+		Onibus o = v.getOnibus();
+		Motorista m = v.getMotorista();
 		
+		Connection c = gDao.getConnection();
+		String sql = "INSERT INTO viagem VALUES (?, ?, ?, ?, ?, ?, ?)";
+		PreparedStatement ps = c.prepareStatement(sql);
+		ps.setInt(1, v.getCodigo());
+		ps.setString(2, o.getPlaca());
+		ps.setInt(3, m.getCodigo());
+		ps.setInt(4, v.getHoraSaida());
+		ps.setInt(5, v.getHoraChegada());
+		ps.setString(6, v.getPartida());
+		ps.setString(7, v.getDestino());
+		ps.execute();
+		ps.close();
+		c.close();
 	}
 
 	@Override
 	public void alterar(Viagem v) throws SQLException, ClassNotFoundException 
 	{
+		Onibus o = v.getOnibus();
+		Motorista m = v.getMotorista();
 		
+		Connection c = gDao.getConnection();
+		String sql = "UPDATE viagem SET "
+				   + "onibus_placa = ?, "
+				   + "motorista_codigo = ?, "
+				   + "hora_saida = ? "
+				   + "hora_chega = ? "
+				   + "partida = ? "
+				   + "destino = ? "
+				   + "WHERE codigo = ?";
+		PreparedStatement ps = c.prepareStatement(sql);
+		ps.setString(1, o.getPlaca());
+		ps.setInt(2, m.getCodigo());
+		ps.setInt(3, v.getHoraSaida());
+		ps.setInt(4, v.getHoraChegada());
+		ps.setString(5, v.getPartida());
+		ps.setString(6, v.getDestino());
+		ps.setInt(7, v.getCodigo());
+		ps.execute();
+		ps.close();
+		c.close();
 	}
 
 	@Override
 	public void excluir(Viagem v) throws SQLException, ClassNotFoundException 
 	{
-		
+		Connection c = gDao.getConnection();
+		String sql = "DELETE viagem WHERE codigo = ?";
+		PreparedStatement ps = c.prepareStatement(sql);
+		ps.setInt(1, v.getCodigo());
+		ps.execute();
+		ps.close();
+		c.close();
 	}
 
 	@Override
 	public Viagem consultar(Viagem v) throws SQLException, ClassNotFoundException 
 	{
-		return null;
+		Connection c = gDao.getConnection();
+		String sql = "SELECT codigo, "
+				   + "onibus_placa, "
+				   + "motorista_codigo, "
+				   + "hora_saida, "
+				   + "hora_chegada, "
+				   + "partida, "
+				   + "destino "
+				   + "FROM viagem WHERE codigo = ?";
+		PreparedStatement ps = c.prepareStatement(sql);
+		ps.setInt(1, v.getCodigo());
+		ResultSet rs = ps.executeQuery();
+		
+		if(rs.next())
+		{
+			Onibus o = new Onibus();
+			Motorista m = new Motorista();
+			
+			v.setCodigo(rs.getInt("codigo"));
+			o.setPlaca(rs.getString("onibus_placa"));
+			v.setOnibus(o);
+			m.setCodigo(rs.getInt("motorista_codigo"));
+			v.setMotorista(m);
+			v.setHoraSaida(rs.getInt("hora_saida"));
+			v.setHoraChegada(rs.getInt("hora_chegada"));
+			v.setPartida(rs.getString("partida"));
+			v.setDestino(rs.getString("destino"));
+		}
+		
+		rs.close();
+		ps.close();
+		c.close();
+		
+		return v;
 	}
 
 	@Override
 	public List<Viagem> list() throws SQLException, ClassNotFoundException 
 	{
-		return null;
+		List<Viagem> viagens = new ArrayList<Viagem>();
+		Connection c = gDao.getConnection();
+		String sql = "SELECT codigo, "
+				   + "onibus_placa, "
+				   + "motorista_codigo, "
+				   + "hora_saida, "
+				   + "hora_chegada, "
+				   + "partida, "
+				   + "destino "
+				   + "FROM viagem";
+		PreparedStatement ps = c.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		
+		while(rs.next())
+		{
+			Onibus o = new Onibus();
+			Motorista m = new Motorista();
+			Viagem v = new Viagem();
+			
+			v.setCodigo(rs.getInt("codigo"));
+			o.setPlaca(rs.getString("onibus_placa"));
+			v.setOnibus(o);
+			m.setCodigo(rs.getInt("motorista_codigo"));
+			v.setMotorista(m);
+			v.setHoraSaida(rs.getInt("hora_saida"));
+			v.setHoraChegada(rs.getInt("hora_chegada"));
+			v.setPartida(rs.getString("partida"));
+			v.setDestino(rs.getString("destino"));
+			
+			viagens.add(v);
+		}
+		
+		rs.close();
+		ps.close();
+		c.close();
+		
+		return viagens;
 	}
 
 }
