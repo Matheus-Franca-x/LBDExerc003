86029ca5638d5f86c93f6ae9206a6af51aecdf2d
Matheus França <matheussss6613@gmail.com>
2024-03-06 04:42:30 -0300

    GenericDao fix

--- src/main/java/persistence/GenericDao.java
+++ src/main/java/persistence/GenericDao.java
@@ -8,14 +8,15 @@ public class GenericDao
 {
 	private Connection c;
 
-    public Connection getConnection() throws SQLException
+    public Connection getConnection() throws SQLException, ClassNotFoundException
     {
         String hostName = "localhost";
         String dbName = "viagem";
         String user = "usuarioDB";
         String password = "123456";
 
-
+        Class.forName("net.sourceforge.jtds.jdbc.Driver");
+        
         c = DriverManager.getConnection(String.format("jdbc:jtds:sqlserver://%s:1433;databaseName=%s;user=%s;password=%s",
                     hostName, dbName, user, password));
 
